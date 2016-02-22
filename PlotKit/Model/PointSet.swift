6b841bc9d6a5a493a836914a2358274e50332642
Alejandro Isaza <aisaza@venturemedia.com>
2016-02-22 15:07:04 -0800

    Improve point set plotting performance

--- PlotKit/Model/PointSet.swift
+++ PlotKit/Model/PointSet.swift
@@ -25,35 +25,60 @@ public struct Point {
 }
 
 public class PointSet {
-    public var points: [Point]
+    public var points: [Point] {
+        didSet {
+            updateIntervals()
+        }
+    }
     public var lineWidth = CGFloat(1.0)
     public var lineColor: NSColor? = NSColor.redColor()
     public var fillColor: NSColor? = nil
     public var pointColor: NSColor? = NSColor.redColor()
     public var pointType = PointType.None
 
-    public var xInterval: ClosedInterval<Double> {
-        let xValues = points.map{ $0.x }
-        if xValues.isEmpty {
-            return 0...0
-        }
-        return xValues.minElement()!...xValues.maxElement()!
-    }
-    public var yInterval: ClosedInterval<Double> {
-        let yValues = points.map{ $0.y }
-        if yValues.isEmpty {
-            return 0...0
-        }
-        return yValues.minElement()!...yValues.maxElement()!
-    }
+    public var xInterval = ClosedInterval<Double>(0, 0)
+    public var yInterval = ClosedInterval<Double>(0, 0)
 
     public init() {
         self.points = []
     }
     public init<T: SequenceType where T.Generator.Element == Point>(points: T) {
         self.points = [Point](points)
+        updateIntervals()
     }
     public init<T: SequenceType where T.Generator.Element == Double>(values: T) {
         self.points = values.enumerate().map{ Point(x: Double($0.0), y: $0.1) }
+        updateIntervals()
+    }
+
+    func updateIntervals() {
+        guard let first = points.first else {
+            xInterval = 0...0
+            yInterval = 0...0
+            return
+        }
+
+        var minX = first.x
+        var maxX = first.x
+        var minY = first.y
+        var maxY = first.y
+
+        for p in points {
+            if p.x < minX {
+                minX = p.x
+            }
+            if p.x > maxX {
+                maxX = p.x
+            }
+            if p.y < minY {
+                minY = p.y
+            }
+            if p.y > maxY {
+                maxY = p.y
+            }
+        }
+
+        xInterval = minX...maxX
+        yInterval = minY...maxY
     }
 }
